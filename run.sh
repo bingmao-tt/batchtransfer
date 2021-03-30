7c6bb78234df973ff1b7c50ea0e3146a9287ab0a
bingmao.wu <bingmao.wu@thundertoken.tw>
2021-03-30 18:06:23 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 DIR_PATH=$(dirname "$(readlink -f "${0}")")
 
-cat "${DIR_PATH}/BatchTransfer/to.csv"
-# docker-compose up -d
+# Check private key file
+if [ ! -f "${DIR_PATH}/BatchTransfer/.private.key" ]; then
+    echo ".private.key don't exist."
+    exit 1
+fi
+
+# Check to.csv
+echo -e "\n### to.csv ###"
+if ! cat "${DIR_PATH}/BatchTransfer/to.csv" 2>/dev/null; then
+    echo "to.csv don't exist."
+    exit 1
+fi
+echo -e "\n"
+
 read -r -s -p "Does to.csv current?: " current ; echo ""
 if [ "${current}" == "yes" ]; then
     echo "Start transfer"
+    # Start execute batchtransfer
     docker run --rm -ti --name transfer --workdir="/BatchTransfer" -v "${DIR_PATH}/BatchTransfer:/BatchTransfer" -v "${DIR_PATH}/transfer.sh:/BatchTransfer/transfer.sh" thundercoretw/batchtransfer:b5c7d4c /BatchTransfer/transfer.sh
+
+    echo -e "\nTransfer done."
 else
     echo "exit!"
 fi

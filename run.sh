49e16129981c981e87ff9003e2439399fb530e2e
bingmao.wu <bingmao.wu@thundertoken.tw>
2021-05-12 17:48:42 +0800

    add transfer failed info.

--- run.sh
+++ run.sh
@@ -86,9 +86,11 @@ read -r -s -p "Does to.csv current?: " current ; echo ""
 if [ "${current}" == "yes" ]; then
     echo "Start transfer"
     # Start execute batchtransfer
-    docker run --rm -ti --name transfer --workdir="/BatchTransfer" -v "${DIR_PATH}/BatchTransfer:/BatchTransfer" -v "${DIR_PATH}/transfer.sh:/BatchTransfer/transfer.sh" thundercoretw/batchtransfer:b5c7d4c /BatchTransfer/transfer.sh
-
-    echo -e "\nTransfer done."
+    if docker run --rm -ti --name transfer --workdir="/BatchTransfer" -v "${DIR_PATH}/BatchTransfer:/BatchTransfer" -v "${DIR_PATH}/transfer.sh:/BatchTransfer/transfer.sh" thundercoretw/batchtransfer:b5c7d4c /BatchTransfer/transfer.sh; then
+        echo -e "\nTransfer done."
+    else
+        echo -e "\nTransfer failed."
+    fi
 else
     echo "exit!"
 fi
